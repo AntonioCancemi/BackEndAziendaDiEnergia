f9403e4e774e994c53fb8cac98d8212957cf67d2
AntonioCancemi <antoniocancemi1@gmail.com>
2023-06-19 10:16:01 +0200

    setup

---
+++ gestionale_anziendale_energia/src/main/java/com/buildweek/gestionale_anziendale_energia/GestionaleAnziendaleEnergiaApplication.java
@@ -0,0 +1,13 @@
+package com.buildweek.gestionale_anziendale_energia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionaleAnziendaleEnergiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionaleAnziendaleEnergiaApplication.class, args);
+	}
+
+}
---
+++ gestionale_anziendale_energia/src/test/java/com/buildweek/gestionale_anziendale_energia/GestionaleAnziendaleEnergiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildweek.gestionale_anziendale_energia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionaleAnziendaleEnergiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
