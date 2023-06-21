f25a070db597448b9568fb90e398d95c1d348749
Eleonora Mattera <m.eleonora98@live.it>
2023-06-21 15:31:16 +0200

    Paginazione

--- gestionale_anziendale_energia/src/main/java/com/buildweek/gestionale_anziendale_energia/repository/ComuneItaliaDaoRepository.java
+++ gestionale_anziendale_energia/src/main/java/com/buildweek/gestionale_anziendale_energia/repository/ComuneItaliaDaoRepository.java
@@ -1,9 +1,15 @@
 package com.buildweek.gestionale_anziendale_energia.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.buildweek.gestionale_anziendale_energia.models.ComuneItalia;
 
-public interface ComuneItaliaDaoRepository extends JpaRepository<ComuneItalia, Long> {
+public interface ComuneItaliaDaoRepository extends JpaRepository<ComuneItalia, Long>, PagingAndSortingRepository<ComuneItalia, Long>{
 
+	Page<ComuneItalia> findByCap(Long cap, Pageable pageable);
+	Page<ComuneItalia> findByProvincia(String provincia, Pageable pageable);
+	
 }
